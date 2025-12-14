a13ae0d8c418c52b3b95cb94769aea30a6d0bf9d
od1000456 <shsaud@ntgclarity.com>
2025-12-14 14:17:05 +0300

    add theme for the app
    # Conflicts:
    #	app/src/main/java/com/example/habittracker/presentation/ui/theme/Theme.kt

--- app/src/main/java/com/example/habittracker/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/example/habittracker/presentation/ui/theme/Color.kt
@@ -2,10 +2,15 @@ package com.example.habittracker.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val PurpleTab = Color(0xFF8C8AF2)
+val PurpleTabLight = Color(0xFFE6E5FF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val RedGoal = Color(0xFFFF9E9E)
+val BlueWork = Color(0xFFD6D9FF)
+val GreenMeditation = Color(0xFFCFF5D9)
+val OrangeBasketball = Color(0xFFFFD2A6)
+val BlueSleep = Color(0xFFCFE3FF)
+
+val AppBackground = Color(0xFFF7F8FC)
+val CardSurface = Color(0xFFFFFFFF)
+
